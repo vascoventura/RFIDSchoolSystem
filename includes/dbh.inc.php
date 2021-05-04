438fb508a6e88572b4ee1c277c66339bb5af95e1
Vasco Ventura <48354062+vascoventura@users.noreply.github.com>
2021-05-04 13:25:45 +0100

    Add files via upload

---
+++ includes/dbh.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+error_reporting(0);
+
+/*$server = "localhost" ;
+$user = "root" ;
+$pass = "" ;
+$db = "projetofinal" ;
+*/
+/*
+$server = "85.10.205.173";
+$user = "vascoventura123";
+$pass = "vascoventura123";
+$db = "finalproject123";
+*/
+
+$server = "localhost";
+$user = "id15316019_vascoventura123" ;
+$pass = "Y!1/X?I4n_bg@R3F" ;
+$db = "id15316019_projetofinal" ;
+
+$conn = mysqli_connect($server, $user, $pass, $db) or die("Unable to Connect!");
+
+
+?>
