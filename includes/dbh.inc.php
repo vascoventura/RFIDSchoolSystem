f08d15551cdf11a1575f05f5cca051050b05be5b
Vasco Ventura <48354062+vascoventura@users.noreply.github.com>
2024-05-04 14:27:17 +0100

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,18 +2,6 @@
 
 error_reporting(0);
 
-/*$server = "localhost" ;
-$user = "root" ;
-$pass = "" ;
-$db = "projetofinal" ;
-*/
-/*
-$server = "85.10.205.173";
-$user = "vascoventura123";
-$pass = "vascoventura123";
-$db = "finalproject123";
-*/
-
 $server = "localhost";
 $user = "id15316019_vascoventura123" ;
 $pass = "Y!1/X?I4n_bg@R3F" ;
